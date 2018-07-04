1dcd9b63a1cc30c8d59cc860ed687f1115de0c96
Tibor Szasz <tszasz@gmail.com>
2018-07-04 02:26:23 +0200

    feat(app): close button

--- assets/main.js
+++ assets/main.js
@@ -2,14 +2,14 @@
 const App = {
     init() {
         console.log("initializing...");
-        this.endpoint = 'http://0af1cd4d.ngrok.io/predict';
+        this.endpoint = 'http://b72c2056.ngrok.io/predict';
         this.webcam = document.querySelector('.webcam');
         this.canvas = document.querySelector('canvas');
         this.video = document.querySelector('video');
         this.ctx = this.canvas.getContext('2d');
         this.addImageUploadEvent();
         this.addCameraEvent();
-        this.addScreenshotEvent();
+        this.addScreenshotEvents();
     },
 
     addImageUploadEvent() {
@@ -22,14 +22,20 @@ const App = {
         button.addEventListener('click', this.cameraInit.bind(this));
     },
 
-    addScreenshotEvent() {
-        const button = document.querySelector('#screenshot');
-        button.addEventListener('click', e => {
+    addScreenshotEvents() {
+        const submit_button = document.querySelector('#screenshot');
+        const close_button = document.querySelector('.webcam .close');
+
+        submit_button.addEventListener('click', e => {
             console.log('sending screenshot to server...');
             this.sendImageData(this.takeScreenshot())
                 .then(response => {
                     console.log(response);
                 })
+        });
+        
+        close_button.addEventListener('click', e => {
+            this.webcam.classList.add('hidden');            
         });
     },
 
